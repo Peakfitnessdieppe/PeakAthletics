dc733519470fc8dc04c77d3d36d0d8500dc07e52
Cullen <cullenfraser81@gmail.com>
2026-09-03 09:09:17 -0300

    fix: admin-set-password use SUPABASE_URL env var

--- netlify/functions/admin-set-password.js
+++ netlify/functions/admin-set-password.js
@@ -6,7 +6,7 @@ exports.handler = async (event) => {
   }
 
   const supabase = createClient(
-    process.env.VITE_SUPABASE_URL,
+    process.env.SUPABASE_URL,
     process.env.SUPABASE_SERVICE_ROLE_KEY
   )
 
